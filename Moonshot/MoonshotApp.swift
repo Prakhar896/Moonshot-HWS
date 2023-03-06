6265acd96af2cbd326fca21f1eccaee90f95495d
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-03-06 22:59:14 +0800

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Prakhar Trivedi on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
